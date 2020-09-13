af2779af77df8e8fc88841e61983b9c4072433cc
3m1n3nc3 <mygames.ng@gmail.com>
2020-09-13 18:11:02 +0100

    Added the database.sql file

---
+++ database.sql
@@ -0,0 +1,103 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+CREATE TABLE IF NOT EXISTS `active_products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int DEFAULT NULL,
+  `domain` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` int NOT NULL DEFAULT '0',
+  `date` int DEFAULT NULL,
+  `updated` int DEFAULT NULL,
+  `deleted` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci; 
+
+CREATE TABLE IF NOT EXISTS `all_products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `uid` int DEFAULT NULL,
+  `name` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `domain` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` int NOT NULL DEFAULT '1',
+  `date` int DEFAULT NULL,
+  `updated` int DEFAULT NULL,
+  `deleted` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci; 
+
+CREATE TABLE IF NOT EXISTS `analytics` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `item_id` int NOT NULL,
+  `uid` int NOT NULL DEFAULT '0',
+  `uip` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `type` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `metric` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `referrer` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `date` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item_id` (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci; 
+
+CREATE TABLE IF NOT EXISTS `notifications` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `notifier_id` int NOT NULL DEFAULT '0',
+  `recipient_id` int NOT NULL DEFAULT '0',
+  `type` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `seen` int DEFAULT NULL,
+  `time` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `recipient_id` (`recipient_id`),
+  KEY `notifier_id` (`notifier_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `setting_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `setting_value` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `settings` (`id`, `setting_key`, `setting_value`) VALUES
+(145, 'theme', 'default'),
+(146, 'site_name', 'Ourzobia PHP API'),
+(147, 'des_disable_sidebar_expand', ' sidebar-no-expand'),
+(148, 'des_fixed_layout', ''),
+(149, 'des_nav_border', ' border-bottom-0'),
+(150, 'des_nav_variant', ' navbar-light navbar-warning'),
+(151, 'des_accent_color_variant', ' accent-warning'),
+(152, 'des_sidebar_variants', ' sidebar-dark-warning'),
+(153, 'des_logo_skin', ' navbar-warning'),
+(154, 'des_body_small_text', ' text-sm'),
+(155, 'des_nav_small_text', ' text-sm'),
+(156, 'des_sidenav_small_text', ' text-sm'),
+(157, 'des_footer_small_text', ' text-sm'),
+(158, 'des_flat_nav', ''),
+(159, 'des_legacy_nav', ''),
+(160, 'des_compact_nav', ''),
+(161, 'des_indent_nav', ' nav-child-indent'),
+(162, 'des_small_brand', ' text-sm');
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `uid` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone_code` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone_number` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fullname` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `admin` int NOT NULL DEFAULT '0',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` int NOT NULL DEFAULT '2',
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `uip` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `reg_time` int NOT NULL,
+  `last_update` int DEFAULT NULL,
+  `deleted` int DEFAULT NULL,
+  PRIMARY KEY (`uid`),
+  KEY `uip` (`uip`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `users` (`uid`, `username`, `email`, `phone_code`, `phone_number`, `fullname`, `avatar`, `admin`, `password`, `status`, `token`, `reg_time`, `last_update`, `deleted`) VALUES
+(1, 'mygames.ng', 'mygames.ng@gmail.com', NULL, NULL, 'Obi Iruruantaziba J', NULL, 3, '$2y$10$DUMpOfHVuTLPCah.IZW5u.qQ2cyd7vLIXnaQoL0..b32okT1cvyCm', 2, '3611456c5101cd95098e184afad6b3fdc2c6b689', 1592235851, 1592235851, NULL);
